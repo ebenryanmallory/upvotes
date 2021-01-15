a824b38de94b0427ca8f8f957a1295875d795e1d
Eben Mallory <ebenryanmallory@gmail.com>
2021-01-14 19:04:49 -0700

    added unstyled tag views

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-        <Features />
+        <Topics />
     </QueryClientProvider>
   );
 }
@@ -28,29 +28,31 @@ function App() {
 function FetchSupabase() {
   return useQuery("supabase", async () => {
     let { data } = await supabase
-    .from('upvote')
-    .select('*')
-    .order('votes', { nullsFirst: false, foreignTable: undefined, ascending: false })
+      .from('upvote')
+      .select('*')
+      .order('votes', { nullsFirst: false, foreignTable: undefined, ascending: false })
     return data;
   });
 }
 
-function Features() {
+function Topics() {
   const { status, data, error, isFetching } = FetchSupabase(); 
   const [ID, setID] = useState(-1);
+  const [filter, setFilter] = useState({ tag: '*', string: '' });
 
   const mutation = useMutation(async ({name, votes}) => {
     await supabase
       .from('upvote')
       .update({ 'votes': votes + 1})
       .eq('feature_name', name)
-      queryClient.invalidateQueries('supabase')
+    // React-query docs recommend a mutation setting to invalidate the cache
+    queryClient.invalidateQueries('supabase')
   });
 
     return (
       <div className="container px-16 py-16">
         <h1 className="text-center text-2xl text-gray-700 my-8 text-uppercase">Look, we got it wrong.</h1>
-        <h4 className="text-center text-gray-700 my-8">We are painfully aware. Help get us on the right track.</h4>
+        <h4 className="text-center text-gray-700 my-8">Help get us on the right track.</h4>
         <div>
           {status === "loading" ? (
             "Loading..."
@@ -58,7 +60,7 @@ function Features() {
             <span>Error: {error.message}</span>
           ) : ID >= 0 ? (
             <div className="border-solid border shadow-sm p-8 my-8">
-              <span className="text-gray-700 hover:text-blue-700 cursor-pointer" onClick={() => setID(-1)}>Back</span>
+              <span className="text-gray-700 hover:text-blue-700 cursor-pointer" onClick={() => setID({ tag: '*', string: '' })}>Back</span>
               <br />
               <span>{data[ID]['feature_name']}</span>
               <br />
@@ -72,6 +74,45 @@ function Features() {
               <br />
               <span>{data[ID]['type']}</span>
             </div>
+          ) : filter.tag === 'status' || filter.tag === 'category' || filter.tag === 'type' ? (
+            data.map((feature) => (
+                <div key={uuidv4()}>
+                  {filter.tag === 'status' && filter.string === feature['status'] && 
+                    <div key={uuidv4()} className="border-solid border shadow-sm p-8 my-8">
+                      <span key={uuidv4()} className="text-gray-700 hover:text-blue-700 cursor-pointer" onClick={() => setFilter({ tag: '*', string: '' })}>Back</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{feature['feature_name']}</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{filter.tag}</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{filter.string}</span>
+                    </div>
+                  }
+                  {filter.tag === 'category' && filter.string === feature['category'] && 
+                    <div key={uuidv4()} className="border-solid border shadow-sm p-8 my-8">
+                      <span key={uuidv4()} className="text-gray-700 hover:text-blue-700 cursor-pointer" onClick={() => setFilter({ tag: '*', string: '' })}>Back</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{feature['feature_name']}</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{filter.tag}</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{filter.string}</span>
+                    </div>
+                  }
+                  {filter.tag === 'type' && filter.string === feature['type'] && 
+                    <div key={uuidv4()} className="border-solid border shadow-sm p-8 my-8">
+                      <span key={uuidv4()} className="text-gray-700 hover:text-blue-700 cursor-pointer" onClick={() => setFilter({ tag: '*', string: '' })}>Back</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{feature['feature_name']}</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{filter.tag}</span>
+                      <br key={uuidv4()} />
+                      <span key={uuidv4()}>{filter.string}</span>
+                    </div>
+                  }
+                </div>
+              )
+            )
           ) : (
             data.map((feature, index) => (
               <div key={uuidv4()} className="border-solid border shadow-sm p-8 my-8">
@@ -87,9 +128,9 @@ function Features() {
                     <span key={uuidv4()} className="rounded" >{feature['votes']}</span>
                   </div>
                   <div key={uuidv4()} className="flex">
-                    <div key={uuidv4()} className="flex-initial ml-0 mx-2 my-4 px-4 bg-pink-300 border border-pink-400 rounded text-pink-700 hover:text-pink-900 cursor-pointer">{feature['status']}</div>
-                    <div key={uuidv4()} className="flex-initial mx-2 my-4 px-4 bg-blue-300 border border-blue-400 rounded text-blue-700 hover:text-blue-900 cursor-pointer">{feature['category']}</div>
-                    <div key={uuidv4()} className="flex-initial mx-2 my-4 px-4 bg-yellow-500 border border-yellow-600 rounded text-yellow-800 hover:text-yellow-900 cursor-pointer">{feature['type']}</div>
+                    <div key={uuidv4()} onClick={() => setFilter({ tag: 'status', string: feature['status'] })} className="flex-initial ml-0 mx-2 my-4 px-4 bg-pink-300 border border-pink-400 rounded text-pink-700 hover:text-pink-900 cursor-pointer">{feature['status']}</div>
+                    <div key={uuidv4()} onClick={() => setFilter({ tag: 'category', string: feature['category'] })} className="flex-initial mx-2 my-4 px-4 bg-blue-300 border border-blue-400 rounded text-blue-700 hover:text-blue-900 cursor-pointer">{feature['category']}</div>
+                    <div key={uuidv4()} onClick={() => setFilter({ tag: 'type', string: feature['type'] })} className="flex-initial mx-2 my-4 px-4 bg-yellow-500 border border-yellow-600 rounded text-yellow-800 hover:text-yellow-900 cursor-pointer">{feature['type']}</div>
                   </div>
                 </div>
                 <span>{isFetching ? "Background Updating..." : " "}</span>
@@ -101,4 +142,4 @@ function Features() {
     );
 }
 
-render(<App />, document.querySelector("#root"));
+render(<App />, document.querySelector("#most-upvotes"));
